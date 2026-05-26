9cdcd91e1c1162e3bde1ac2ce14a8f6e8d5650d3
0xlam <208283706+0xlam@users.noreply.github.com>
2026-05-26 06:27:20 +0100

    Update __init__.py

--- src/phishsage/handlers/__init__.py
+++ src/phishsage/handlers/__init__.py
@@ -1,1 +1,9 @@
-# handlers/__init__.py
+from .attachments import handle_attachments
+from .links import handle_links
+from .headers import handle_headers
+
+__all__ = [
+    "handle_attachments",
+    "handle_links",
+    "handle_headers",
+]
---
+++ src/phishsage/outputs/printer/__init__.py
@@ -0,0 +1,30 @@
+from .shared import print_warning, print_error
+from .headers import print_header_heuristics
+from .attachments import (
+    print_attachment_listing,
+    print_attachment_extraction,
+    print_attachment_hashes,
+    print_vt_scan_attachments,
+    print_yara_scan_attachments,
+)
+from .links import (
+    print_url_extraction,
+    print_vt_scan_links,
+    print_redirect_chain,
+    print_link_heuristics,
+)
+
+__all__ = [
+    "print_warning",
+    "print_error",
+    "print_header_heuristics",
+    "print_attachment_listing",
+    "print_attachment_extraction",
+    "print_attachment_hashes",
+    "print_vt_scan_attachments",
+    "print_yara_scan_attachments",
+    "print_url_extraction",
+    "print_vt_scan_links",
+    "print_redirect_chain",
+    "print_link_heuristics",
+]
